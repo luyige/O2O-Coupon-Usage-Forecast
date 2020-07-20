7a32e0525b8bcf9e3de4282a928447f1700f352d
luyige <365260158@qq.com>
2020-07-20 14:16:38 +0800

    a_test

--- code/tianyin/java src/o2o/src/a_test.java
+++ code/tianyin/java src/o2o/src/a_test.java
@@ -2,5 +2,11 @@ public class a_test {
     public static void main(String[] args) {
         System.out.print("a");
         System.out.print("b");
+        System.out.print("c");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
